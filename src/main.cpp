503ab5e7bc4a0c4eb7fce237e0de01eec7e4bcd8
Pashan Irani <pashanirani@outlook.com>
2022-03-16 20:05:32 -0700

    project setup

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "test.h"
+int main(int argc, char const *argv[])
+{
+  printf("Hello World!\n");
+  testPrint();
+  return 0;
+}
---
+++ src/test.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void testPrint() {
+  printf("test\n");
+}
